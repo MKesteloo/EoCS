77f49ba5e1312d8f02f335a3988b40a914700299
rubenmeeuw <r.d.meeuwissen@student.tudelft.nl>
2018-09-23 17:02:07 +0200

    Added Domain plot R script

--- Domains.R
+++ Domains.R
@@ -4,9 +4,10 @@ ransomwareDataset <- read.csv(file="dataset_ransomware.csv", header=TRUE, sep=",
 # Create domain column
 domain <- function(x){
   if(x!=""){
-      splited <- strsplit(strsplit(gsub("http://|https://|www\\.", "", x), "/")[[c(1, 1)]], ".", fixed = TRUE)
-      if(length(splited[[1]]) == 2){
-        splited[[c(1,2)]]
+      splitted <- strsplit(strsplit(gsub("http://|https://|www\\.", "", x), "/")[[c(1, 1)]], ".", fixed = TRUE)
+      lastelem <- tail(splitted[[1]], n=1)
+      if(is.na(as.numeric(lastelem))){
+        lastelem
       }
       else{
         'unknown'
@@ -14,6 +15,18 @@ domain <- function(x){
   }
 }
 
-ransomwareDataset$Host <- lapply(ransomwareDataset$Host, domain)
-#domainfreq <- as.data.frame(table(ransomwareDataset$Host))
-lol <- ransomwareDataset$Host
+splitedvec <- unlist(lapply(ransomwareDataset$Host, domain), use.names=FALSE)
+
+domainfreq <- as.data.frame(table(splitedvec))
+names(domainfreq)[1] = "Domains"
+
+# Order on size and pick the top 20
+orderedFreq <- domainfreq[with(domainfreq, order(Freq, decreasing = TRUE)), ]
+top20 <- orderedFreq[1:20,]
+rownames(top20) <- NULL
+top20 <- droplevels(top20)
+
+# Plot a graph
+bp <- barplot(top20$Freq, names.arg = top20$Domain, cex.names = 0.5, las = 1, horiz = TRUE, xlim=c(0,6400))
+values <- as.matrix(top20$Freq)
+text(values+120, bp, labels=as.character(values), cex = 0.5)
